ce18e8a92e5b226e29b25b703b1e0cee24b6f4a8
ulysses497 <2420770369@qq.com>
2020-02-21 01:27:25 +0800

    111111

--- src/com/test2/Firsttest2.java
+++ src/com/test2/Firsttest2.java
@@ -6,11 +6,11 @@ public class Firsttest2 {
     private String test5;
     private String test6;
     private String testbranch;
-    public String getTest2() {
-        return test2;
+    public String getTest11() {
+        return test11;
     }
 
     public void setTest2(String test2) {
-        this.test2 = test2;
+        this.test11 = test2;
     }
 }
