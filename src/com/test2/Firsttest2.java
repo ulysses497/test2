5ae38d8c478a1944c49ac28b445147fae0d5b813
ulysses497 <2420770369@qq.com>
2020-02-21 01:18:36 +0800

    Merge branch 'master' of https://github.com/ulysses497/test2
    # Conflicts:
    #	src/com/test2/Firsttest2.java

--- src/com/test2/Firsttest2.java
+++ src/com/test2/Firsttest2.java
@@ -5,6 +5,7 @@ public class Firsttest2 {
     private String test3;
     private String test5;
     private String test6;
+    private String testbranch;
     public String getTest2() {
         return test2;
     }
